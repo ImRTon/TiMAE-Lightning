ae7ab811aa5e08307806951c30bf4a05c3ea479e
ImRTon <col360785@gmail.com>
2024-07-25 05:01:40 +0000

    fix: UCR dataset bugs

--- network/dataset.py
+++ network/dataset.py
@@ -44,7 +44,7 @@ class UCR2018(L.LightningDataModule):
         self.class_id_start = class_id_start
 
         if class_weights is None:
-            class_counts = self.df.iloc[:, 0].value_counts()
+            class_counts = self.df.iloc[:, 0].value_counts().sort_index()
             class_freq = class_counts / class_counts.sum() * num_classes
 
             class_weights = 1.0 / class_freq
@@ -79,12 +79,13 @@ class UCR2018(L.LightningDataModule):
             else:
                 self.train_df = self.df
 
+            self.train_label = self.train_df.pop(0).to_numpy() - self.class_id_start
+            self.val_label = self.val_df.pop(0).to_numpy() - self.class_id_start
+
             if self.hparams.normalize:
                 self.train_df = (self.train_df - self.hparams.mean) / self.hparams.std
                 self.val_df = (self.val_df - self.hparams.mean) / self.hparams.std
 
-            self.train_label = self.train_df.pop(0).to_numpy() - self.class_id_start
-            self.val_label = self.val_df.pop(0).to_numpy() - self.class_id_start
             self.train_df = self.train_df.to_numpy()
             self.val_df = self.val_df.to_numpy()
             
@@ -168,3 +169,137 @@ class UCR2018Dataset(Dataset):
             return torch.tensor(data, dtype=torch.float32).unsqueeze(1)
         else:
             return torch.tensor(data, dtype=torch.float32).unsqueeze(1), torch.tensor(self.label[idx], dtype=torch.int64)
+        
+class KaggleECGCategorization(L.LightningDataModule):
+    def __init__(
+        self, 
+        batch_size: int,
+        normalize: bool,
+        dataset_path: str | Path,
+        val_dataset_path: str | Path | None = None,
+        split_ratio: float = 0.85,
+        crop_size: int | None = None,
+        pretrain: bool = False,
+        std: float | None = None,
+        mean: float | None = None,
+        num_classes: int | None = None,
+        class_weights: list[float] | None = None
+    ):
+        super().__init__()
+
+        if isinstance(dataset_path, str):
+            dataset_path = Path(dataset_path)
+        self.dataset_path = dataset_path
+        self.split_ratio = split_ratio
+
+        self.df = pd.read_csv(dataset_path, header=None)
+
+        if num_classes is None:
+            num_classes = len(self.df.iloc[:, -1].value_counts())
+        self.num_classes = num_classes
+
+        if class_weights is None:
+            class_counts = self.df.iloc[:, -1].value_counts().sort_index()
+            class_freq = class_counts / class_counts.sum() * num_classes
+
+            class_weights = 1.0 / class_freq
+            class_weights = class_weights / class_weights.sum() * num_classes
+
+            class_weights = [max(0.1, min(w, 10)) for w in class_weights]
+        self.class_weights = class_weights
+
+        if normalize:
+            if std is None:
+                std = float(self.df.values.std(ddof=1))
+            if mean is None:
+                mean = float(self.df.mean().mean())
+            print(f"Using Mean: {mean}, Std: {std}")
+
+        if val_dataset_path is not None:
+            if isinstance(val_dataset_path, str):
+                val_dataset_path = Path(val_dataset_path)
+            self.val_df = pd.read_csv(val_dataset_path, header=None)
+        else:
+            self.val_df = None
+
+        self.save_hyperparameters()
+
+    def setup(self, stage: str):
+        if stage == 'fit':
+            # Split dataset into train and validation if needed
+            
+            if self.val_df is None:
+                self.train_df, self.val_df = train_test_split(
+                    self.df, train_size=self.split_ratio, random_state=42)
+            else:
+                self.train_df = self.df
+
+            self.train_label = self.train_df.pop(self.train_df.columns[-1]).to_numpy()
+            self.val_label = self.val_df.pop(self.val_df.columns[-1]).to_numpy()
+
+            if self.hparams.normalize:
+                self.train_df = (self.train_df - self.hparams.mean) / self.hparams.std
+                self.val_df = (self.val_df - self.hparams.mean) / self.hparams.std
+
+            self.train_df = self.train_df.to_numpy()
+            self.val_df = self.val_df.to_numpy()
+            
+        elif stage == 'test':
+            self.test_label = self.df.pop(self.df.columns[-1]).to_numpy()
+            self.test_df = self.df.to_numpy()
+
+        elif stage == 'predict':
+            self.predict_label = self.df.pop(self.df.columns[-1]).to_numpy()
+            self.predict_df = self.df.to_numpy()
+
+    def train_dataloader(self):
+        return DataLoader(
+            UCR2018Dataset(
+                self.train_df, 
+                self.train_label, 
+                self.hparams.pretrain,
+                self.hparams.crop_size
+            ), 
+            batch_size=self.hparams.batch_size, 
+            shuffle=True,
+            num_workers=20
+        )
+    
+    def val_dataloader(self):
+        return DataLoader(
+            UCR2018Dataset(
+                self.val_df, 
+                self.val_label, 
+                self.hparams.pretrain,
+                self.hparams.crop_size
+            ), 
+            batch_size=self.hparams.batch_size, 
+            shuffle=False,
+            num_workers=20
+        )
+    
+    def test_dataloader(self):
+        return DataLoader(
+            UCR2018Dataset(
+                self.test_df, 
+                self.test_label, 
+                self.hparams.pretrain,
+                self.hparams.crop_size
+            ), 
+            batch_size=self.hparams.batch_size, 
+            shuffle=False,
+            num_workers=20
+        )
+    
+    def predict_dataloader(self):
+        return DataLoader(
+            UCR2018Dataset(
+                self.predict_df, 
+                self.predict_label, 
+                self.hparams.pretrain,
+                self.hparams.crop_size
+            ), 
+            batch_size=self.hparams.batch_size, 
+            shuffle=False,
+            num_workers=20
+        )
